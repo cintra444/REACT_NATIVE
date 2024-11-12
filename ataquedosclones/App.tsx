3fb7302aebac2d5265c1678408994f1a633c570b
Eber Cintra <organico_51@hotmail.com>
2024-11-12 14:03:38 -0300

    primeira pagina pronta

--- ataquedosclones/App.tsx
+++ ataquedosclones/App.tsx
@@ -1,36 +1,201 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text, FlatList, ScrollView, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
+import logo from './src/assets/logo.png';
+import olho from './src/assets/olho.png';
+import lupa from './src/assets/lupa.png';
+import usuario from './src/assets/usuario.png';
+import pix from './src/assets/pix.png';
+import pagar from './src/assets/pagar.png';
+import cartao from './src/assets/cartao.png';
+import bandeira from './src/assets/bandeira.png'
+import mundo from './src/assets/mundo.png';
+import loop from './src/assets/loop.png';
+import oferta from './src/assets/oferta.png';
+import invest from './src/assets/invest.png';
+import pixcredit from './src/assets/pixcredit.png';
+import recarga from './src/assets/recarga.png';
+import gift from './src/assets/gift.png';
+import poupanca from './src/assets/poupanca.png';
+import pontinhos from './src/assets/pontinhos.png';
+import propaganda from './src/assets/propaganda.png';
+import propaganda2 from './src/assets/propaganda2.png';
+import seta from './src/assets/seta.png';
+import amigos from './src/assets/amigos.png';
+import perfil from './src/assets/perfil.png';
+import porquinho from './src/assets/porquinho.png';
+import moedaLoop from './src/assets/moedaLoop.png'
+import cupomDesconto from './src/assets/cupomDesconto.png';
+import inter from './src/assets/inter.png';
+import inicio from './src/assets/inicio.png';
+
 
 export default function App() {
-    return <View style={styles.container} >
+
+  const menuItems = [
+    { id: '1', title: 'Shopping', image: oferta },
+    { id: '2', title: 'Invest', image: invest },
+    { id: '3', title: 'Pix no crédito', image: pixcredit },
+    { id: '4', title: 'Recarga', image: recarga },
+    { id: '5', title: 'Gift card', image: gift },
+    { id: '6', title: 'Poupança', image: poupanca },
+    { id: '7', title: 'Mostrar mais', image: pontinhos },
+  ];
+
+  return <View style={styles.container}>
+  <ScrollView contentContainerStyle={styles.contentContainer}>
+    
+      <View style={styles.headerTopo}>
+        <Image source={logo} style={styles.iconImageLogo} />
+        <Image></Image>
+        <Image source={olho} style={styles.iconImage} />
+        <Image source={lupa} style={styles.iconImage} />
+        <Image source={usuario} style={styles.iconImage} />
+      </View>
+      {/* card financas */}
       <View style={styles.header}>
-        <Image />
-        <Image />
-        <Image />
-        <Image />
+        <View style={styles.opcoes}>
+          <Image source={bandeira} style={styles.iconImage} />
+          <Text style={styles.iconText}>R$</Text>
+        </View>
+        <View style={styles.opcoes}>
+          <Image source={mundo} style={styles.iconImage} />
+          <Text style={styles.iconText}>US$</Text>
+        </View>
+        <View style={styles.opcoes}>
+          <Image source={loop} style={styles.iconImage} />
+          <Text style={styles.iconText}>Loop</Text>
+        </View>
+        <Image></Image>
       </View>
       <View style={styles.cardValor}>
-        <Image />
-        <Image />
-        <Image />
+        <View style={styles.cardSeta}>
+          <Text style={styles.textValor}>R$ 0,65</Text>
+          <Image source={seta} style={styles.textValor} />
+        </View>
+        {/* card valor */}
+        <View style={styles.cardBody}>
+          <View style={styles.iconRow}>
+            <View style={styles.opcoes}>
+              <Image source={pix} style={styles.iconImageCard} />
+              <Text style={styles.iconText}>Pix</Text>
+            </View>
+          </View>
+          <View style={styles.iconRow}>
+            <View style={styles.opcoes}>
+              <Image source={pagar} style={styles.iconImageCard} />
+              <Text style={styles.iconText}>Pagar</Text>
+            </View>
+          </View>
+          <View style={styles.iconRow}>
+            <View style={styles.opcoes}>
+              <Image source={cartao} style={styles.iconImageCard} />
+              <Text style={styles.iconText}>Cartões</Text>
+            </View>
+          </View>
+        </View>
       </View>
-      <View style={styles.cardMenu}>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
+      {/* card menu */}
+      <View style={styles.menuContainer}>
+        <FlatList
+          data={menuItems}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.cardItem}>
+              <Image source={item.image} style={styles.iconImage} />
+              <Text style={styles.iconTextMenu}>{item.title}</Text>
+            </View>
+          )}
+        />
+      </View>
+
+      <View style={styles.cardAdvertising}>
+        <Image source={propaganda} style={styles.itemPropaganda} />
       </View>
       <View style={styles.cardAdvertising}>
-        <Image/>
+        <Image source={propaganda2} style={styles.itemPropaganda} />
       </View>
-      <View style={styles.cardAdvertising_cnn}>
-        <Image />
+      {/* limite de credito */}
+      <View style={styles.limiteCredito}>
+        <Text style={styles.limiteTitulo}>Limite de Crédito</Text>
+        <View style={styles.limiteValores}>
+          <Text style={styles.limiteTexto}>R$ 0,00</Text>
+          <Text style={styles.limiteTexto}>R$ 3.900,00</Text>
+        </View>
+        <View style={styles.barraSeparadora}></View>
+        <View style={styles.limiteStatus}>
+          <Text style={styles.limiteUtilizado}>Utilizado</Text>
+          <Text style={styles.limiteDisponivel}>Disponível</Text>
+        </View>
       </View>
-
-
+      {/* area da Advertising */}
+      <View style={styles.advertisingContainer}>
+        <View style={styles.cardAdvertisingFim}>
+          <Text style={styles.textAdvertisingTopo}>Meu Porquinho</Text>
+          <Text style={styles.textAdvertisingBody}>Bora guardar</Text>
+          <Text style={styles.textAdvertisingBody}>dinheiro?</Text>
+          <Image source={porquinho} style={styles.cardAdvertisingFimD} />
+        </View>
+        <View style={styles.cardAdvertisingFim}>
+          <Text style={styles.textAdvertisingTopo}>Shopping</Text>
+          <Image source={cupomDesconto}></Image>
+          <View style={styles.desconto}>
+            <Text style={styles.textDesconto}>cupom de desconto</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.advertisingContainer}>
+        <View style={styles.cardAdvertisingFim}>
+          <Text style={styles.textAdvertisingTopo}>Pontos Loop</Text>
+          <Text style={styles.textAdvertisingBody}>0</Text>
+          <Image source={moedaLoop} style={styles.cardAdvertisingFimD} />
+        </View>
+        <View style={styles.cardAdvertisingFim}>
+          <Text style={styles.textAdvertisingTopo}>Evoluir Perfil</Text>
+          <Image source={perfil} />
+          <Text style={styles.textAdvertisingFim}>Seja Inter One</Text>
+          <Text style={styles.textAdvertisingTopo}>Mais beneficios</Text>
+        </View>
+      </View>
+      <View style={styles.advertisingContainer}      >
+        <View style={styles.cardAdvertisingFim}>
+          <Text style={styles.textAdvertisingTopo}>Indique amigos</Text>
+          <Image source={amigos} />
+          <View style={styles.ganheAte}>
+            <Text style={styles.ganhe}>Ganhe até:</Text>
+          </View>
+          <View>
+            <Text style={styles.textAdvertisingBody}>40.000 pts</Text>
+          </View>
+        </View>
+      </View>
+      
+      </ScrollView>
+      {/* rodape fixo */}
+      <View style={styles.menuRodape}>
+        <TouchableOpacity >
+          <Image source={inicio} style={styles.iconImage} />
+          <Text style={styles.textoBotao}>Início</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Image source={invest} style={styles.iconImage} />
+          <Text style={styles.textoBotao}>Invest</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Image source={inter} style={styles.iconImage} />
+          <Text style={styles.textoBotao}>Inter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Image source={oferta} style={styles.iconImage} />
+          <Text style={styles.textoBotao}>Oferta</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Image source={pontinhos} style={styles.iconImage} />
+          <Text style={styles.textoBotao}>Todos</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-
-
+  
 };
--- ataquedosclones/styles.ts
+++ ataquedosclones/styles.ts
@@ -3,22 +3,247 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         backgroundColor: '#232325',
-        flex: 1,
         alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingBottom: 100,
+    },
+    contentContainer: {
+        paddingBottom: 20,
+    },
+    headerTopo: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingTop: 50,
+        padding: 10,
     },
     header: {
-        
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingTop: 10,
+        padding: 10,
     },
     cardValor: {
-
+        width: '100%',
+        height: 180,
+        padding: 20,
+        borderRadius: 15,
+        backgroundColor: '#2d2e30',
+        elevation: 3,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+        marginBottom: 16,
     },
-    cardMenu: {
-
+    cardSeta: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    textValor: {
+        fontSize: 27,
+        fontWeight: 'bold',
+        color: '#fff',
+        textAlign: 'center',
+        marginBottom: 8,
+    },
+    iconRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 8,
+        backgroundColor: '#232325',
+        borderRadius: 30,
+        width: '28%',
+        alignItems: 'center'
+    },
+    iconText: {
+        fontSize: 12,
+        color: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    opcoes: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    cardBody: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingTop: 40,
+    },
+    iconImage: {
+        width: 40,
+        height: 40,
+        resizeMode: 'contain',
+    },
+    iconImageCard: {
+        width: 24,
+        height: 24,
+        resizeMode: 'contain',
+    },
+    iconImageLogo: {
+        resizeMode: 'contain',
+    },
+    menuContainer: {
+        width: '100%',
+        marginTop: 20,
+    },
+    cardItem: {
+        alignItems: 'center',
+        marginHorizontal: 10,
+        backgroundColor: '#2d2e30',
+        padding: 10,
+        borderRadius: 8,
+        height: 65,
+    },
+    iconTextMenu: {
+        fontSize: 12,
+        color: '#fff',
+        alignItems: 'center',
+        marginTop: 20,
     },
     cardAdvertising: {
-
+        marginTop: 20,
+        width: '100%',
+        height: 200,
+        borderRadius: 15,
+        alignItems: 'center',
+        overflow: 'hidden',
     },
-    cardAdvertising_cnn: {
-
+    itemPropaganda: {
+        width: '100%',
+        height: 198,
+        resizeMode: 'contain',
+    },
+    limiteCredito: {
+        backgroundColor: '#2d2e30',
+        width: '100%',
+        padding: 20,
+        borderRadius: 15,
+        alignItems: 'center',
+        marginBottom: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+        elevation: 4,
+    },
+    limiteTitulo: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 10,
+    },
+    limiteValores: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: 10,
+    },
+    barraSeparadora: {
+        height: 10,
+        width: '100%',
+        backgroundColor: '#333338',
+        marginHorizontal: 10,
+        borderRadius: 5,
+    },
+    limiteTexto: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+    limiteStatus: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+    },
+    limiteUtilizado: {
+        fontSize: 12,
+        color: '#fff',
+    },
+    limiteDisponivel: {
+        fontSize: 12,
+        color: '#fff',
+    },
+    cardAdvertisingFim: {
+        backgroundColor: '#2d2e30',
+        width: '45%',
+        height: 140,
+        borderRadius: 12,
+        paddingStart: 10,
+    },
+    cardAdvertisingFimD: {
+        width: 50,
+        height: 50,
+        marginLeft: 85,
+        marginBottom: 5,
+        marginVertical: 10,
+    },
+    textAdvertisingTopo: {
+        color: '#fff',
+        fontSize: 12,
+        width: '100%',
+        alignItems: 'center',
+        marginBottom: 10,
+        margin: 5,
+    },
+    textAdvertisingBody: {
+        color: '#fff',
+        fontSize: 14,
+        fontWeight: 'bold',
+        alignItems: 'flex-start'
+    },
+    textAdvertisingFim: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 12,
+    },
+    advertisingContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 8,
+        color: '#fff',
+        gap: 20,
+    },
+    desconto: {
+    backgroundColor: '#3c2014', 
+    borderRadius: 10,           
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    marginTop: 5, 
+    width: '90%'  
+    },
+    textDesconto: {
+        fontSize: 12,
+        color: '#ad571a',
+    },
+    ganhe: {
+        color: '#ad571a',
+        fontSize: 10,
+        margin: 5,
+    },
+       menuRodape: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: '#2d2e30',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderColor: '#2d2e30',
+        borderWidth: 1,
+        height: 100,
+        
+    },
+    textoBotao: {
+        fontSize: 12,
+        marginTop: 5,
+        color: '#fff'
     }
 });
